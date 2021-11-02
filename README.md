ecbc2965688d25f149a336a869f8a255c5e253a5
spuun <thisisartunion@gmail.com>
2021-11-02 19:21:46 +0700

    Update a bunch of things for quality of life

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Kek's welcome Shiz
+# ✨ Kek's welcome Shiz ✨
+> Any code within this repository and any derivatives thereof are to be licensed with AGPLv3 under kekboi, Art Union org. 
+> see LICENSE.md for details.
+
+## Dependencies
+As we're running `d.js` v13 for this, it *is* ***required*** for you to install `node.js` v16 to run this module as is.
+However, you *can* remove all v13 changes and change the `package.json` to run this on v12.
+
+## How it works
+This repo contains kek's welcoming module for Art Union, what it does is it:
+1.) Gets an introduction message
+2.) Grabs a webhook
+3.) Sends a welcome ping and asks the ping role to welcome the new person
+Simple enough, lmao.
+
+## Usage
+`mv` or rename `.env copy` to `.env` and change the variables accordingly.
--- bot.js
+++ bot.js
@@ -4,7 +4,9 @@ const procenv = process.env,
   client = new Client({
     intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_WEBHOOKS"],
   }),
-  pkg = require("./package.json");
+  pkg = require("./package.json"),
+  gura = procenv.WEBURL.split("/webhooks/").pop(),
+  froot = procenv.OTHERWEBURL.split("/webhooks/").pop();
 
 function logger(string = "logger logging") {
   console.log(`${new Date()}: ${string}`);
@@ -35,22 +37,33 @@ client.on("message", (message) => {
         `Hey <@${authid}>, welcome!`,
         `Yello there <@${authid}>`,
       ];
-    client.fetchWebhook(procenv.WEBID, procenv.WEBTOKEN).then((web) => {
+    client.fetchWebhook(gura.split("/")[0], gura.split("/")[1]).then((web) => {
       web
         .send({ content: welcome[Math.floor(Math.random() * 6)] })
         .then((m) => {
           if (m) {
             logger(`sent welcome message for ${message.author.tag}`);
           }
+        })
+        .catch((e) => {
+          logger(`failed to send ping to role, reason:\n"${e}"`);
         });
     });
     client
-      .fetchWebhook(procenv.OTHERWEBID, procenv.OTHERWEBTOKEN)
+      .fetchWebhook(froot.split("/")[0], froot.split("/")[1])
       .then((web) => {
-        web.send({
-          content: `<@&${procenv.ROLEID}> ples emoclew ${message.author.tag}`,
-        });
-        logger(`sent ping to role`)
+        web
+          .send({
+            content: `<@&${procenv.ROLEID}> ples emoclew ${message.author.tag}`,
+          })
+          .then((m) => {
+            if (m) {
+              logger(`sent ping to role`);
+            }
+          })
+          .catch((e) => {
+            logger(`failed to send ping to role, reason:\n"${e}"`);
+          });
       });
   }
 });
