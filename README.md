b62d5c60d4f501c092cf61eb1d1d1ecd61c94fb6
spuun <thisisartunion@gmail.com>
2021-11-02 19:39:26 +0700

    Add LICENSE, fix README formatting

--- README.md
+++ README.md
@@ -4,13 +4,18 @@
 
 ## Dependencies
 As we're running `d.js` v13 for this, it *is* ***required*** for you to install `node.js` v16 to run this module as is.
+
 However, you *can* remove all v13 changes and change the `package.json` to run this on v12.
 
 ## How it works
 This repo contains kek's welcoming module for Art Union, what it does is it:
+
 1.) Gets an introduction message
+
 2.) Grabs a webhook
+
 3.) Sends a welcome ping and asks the ping role to welcome the new person
+
 Simple enough, lmao.
 
 ## Usage
--- bot.js
+++ bot.js
@@ -1,3 +1,20 @@
+/*
+        Kek's welcoming module for AU.
+        Copyright (C) 2021 kekboi, Art Union org.
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Affero General Public License as
+        published by the Free Software Foundation, version 3 of the License only.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Affero General Public License for more details.
+
+        You should have received a copy of the GNU Affero General Public License
+        along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 require("dotenv").config();
 const procenv = process.env,
   Client = require("discord.js").Client,
